fe44df9f43572c9885ef03e39313e8d6f62b33b8
odobok <katrina.kennett@gmail.com>
2018-10-28 15:41:09 -0600

    futzing with visuals for example to Lauren

--- do-to-app/src/Groups.js
+++ do-to-app/src/Groups.js
@@ -2,6 +2,7 @@ import React from 'react';
 import person from './icon-1.png';
 import pair from './icon-2.png';
 import threesome from './icon-3.png';
+import self from './icon-person.png';
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 
@@ -13,16 +14,20 @@ const Groups = () => (
         <h4>Start a New Group</h4>
       </parsing-groups>
       <parsing-groups className="Groups-parsing">
+        <img src={self} className="icon" alt="one-person" />
+        <h4>My group of just me</h4>
+      </parsing-groups>
+      <parsing-groups className="Groups-parsing">
         <img src={pair} className="icon" alt="two-people" />
         <h4>Research Partnership in Science class</h4>
       </parsing-groups>
       <parsing-groups className="Groups-parsing">
-        <img src={threesome} className="icon" alt="three-people" />
-        <h4>My group of three in English class</h4>
+        <img src={pair} className="icon" alt="two-people" />
+        <h4>Partner work for Music Project</h4>
       </parsing-groups>
       <parsing-groups className="Groups-parsing">
-        <img src={pair} className="icon" alt="two-people" />
-        <h4>Research Partnership for Music Project</h4>
+        <img src={threesome} className="icon" alt="three-people" />
+        <h4>Community research project</h4>
       </parsing-groups>
       <parsing-groups className="Groups-parsing">
         <img src={threesome} className="icon" alt="three-people" />
